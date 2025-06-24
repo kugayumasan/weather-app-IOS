cb96bfe10a42919e8380367ba672e71924f4f38a
kugayumasan <erosferri@hotmail.com>
2025-06-24 13:43:28 +0200

    started routing and installed eveyrthing also tanstack query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import './styles/index.css';
 
-createRoot(document.getElementById('root')).render(
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
